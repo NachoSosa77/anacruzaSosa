6f1e8b22d35c934680e5f616083ad699f9eecf1b
Nacho Sosa <nachopb77@gmail.com>
2022-03-23 01:09:43 -0300

    Corrección card Detail.

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { Card, Button } from "react-bootstrap";
+import React from "react";
+import { Card} from "react-bootstrap";
+import "./ItemDetail.css";
 //import ItemCount from "components/ItemCount/ItemCount";
 
-const ItemDetail = ({id, pictureUrl, title, description, price}) => {
- 
+const ItemDetail = ({ id, pictureUrl, title, description, price }) => {
   return (
-    <div><Card className="card-list">
-    <Card.Header>{id}</Card.Header>
-    <Card.Img variant="top" src={pictureUrl} />
-    <Card.Body>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>{description}</Card.Text>
-    </Card.Body>
-    <Card.Footer className="text-muted">U$D {price}</Card.Footer>
-  </Card></div>
-  )
-}
+    <div className="card-detail">
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>{id}</Card.Header>
+        <Card.Img variant="top" src={pictureUrl} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">U$D {price}</Card.Footer>
+      </Card>
+    </div>
+  );
+};
 
-export default ItemDetail
+export default ItemDetail;
