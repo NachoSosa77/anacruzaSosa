eb6f74b4f1f78b42d4b4b31e03c4ccd8facf549b
Nacho Sosa <nachopb77@gmail.com>
2022-04-25 23:10:32 -0300

    Final

--- src/components/Checkout/Thankyou.js
+++ src/components/Checkout/Thankyou.js
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Card, Button, Container } from "react-bootstrap";
 
-
-export const ThankYou = ({order}) => {
-
-    return <div className="container my-5">
-                <h2>Gracias por tu compra!</h2>
-                <hr/>
-                <h3>Tu número de orden es: {order}</h3>
-                <Link to="/" className="btn btn-primary">Volver</Link>
-            </div>
-}
+export const ThankYou = ({ order }) => {
+  return (
+    <Container className="cart-thank">
+      <Card style={{ width: "50rem" }}>
+            <Card.Header>Anacrusa</Card.Header>
+                    <Card.Body className="text-center">
+                        <Card.Title >
+                        ¡Muchas gracias por tu compra!
+                        </Card.Title>
+                            <Card.Text>
+                                Tu número de orden es: {order}
+                            </Card.Text>
+                                <Link to="/">
+                                    <Button className="btn-idetail" variant="outline-dark">
+                                    Volver
+                                    </Button>
+                                </Link>
+                        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
